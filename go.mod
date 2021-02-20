ef9b903b8470ec478d2a3ddec6ced749a2c64d2a
Guillaume Ballet <gballet@gmail.com>
2021-02-20 12:19:07 +0100

    Update go-kzg to recover GenerateSetup

--- go.mod
+++ go.mod
@@ -6,6 +6,6 @@ require (
 	github.com/ethereum/go-ethereum v1.9.25
 	github.com/herumi/bls-eth-go-binary v0.0.0-20210218073812-92395a189ef4 // indirect
 	github.com/kilic/bls12-381 v0.1.0 // indirect
-	github.com/protolambda/go-kzg v0.0.0-20210218205934-59c32db6a9c1
-	golang.org/x/sys v0.0.0-20210218155724-8ebf48af031b // indirect
+	github.com/protolambda/go-kzg v0.0.0-20210220063554-7b02600fc0c8
+	golang.org/x/sys v0.0.0-20210220050731-9a76102bfb43 // indirect
 )
--- proof.go
+++ proof.go
@@ -32,24 +32,6 @@ import (
 	"github.com/protolambda/go-kzg/bls"
 )
 
-func generateSetup(secret string, n uint64) ([]bls.G1Point, []bls.G2Point) {
-	var s bls.Fr
-	bls.SetFr(&s, secret)
-
-	var sPow bls.Fr
-	bls.CopyFr(&sPow, &bls.ONE)
-
-	s1Out := make([]bls.G1Point, n, n)
-	s2Out := make([]bls.G2Point, n, n)
-	for i := uint64(0); i < n; i++ {
-		bls.MulG1(&s1Out[i], &bls.GenG1, &sPow)
-		bls.MulG2(&s2Out[i], &bls.GenG2, &sPow)
-		var tmp bls.Fr
-		bls.CopyFr(&tmp, &sPow)
-		bls.MulModFr(&sPow, &tmp, &s)
-	}
-	return s1Out, s2Out
-}
 
 func calcR(cs []bls.G1Point, indices []bls.Fr, ys []bls.Fr) bls.Fr {
 	digest := sha256.New()
