281d8982c3b7f26aded563da2e6d22b2f56a2587
Guillaume Ballet <gballet@gmail.com>
2021-03-05 09:54:16 +0100

    [broken] share the failing test with proto

--- proof_test.go
+++ proof_test.go
@@ -52,3 +52,28 @@ func TestProofGenerationTwoLeaves(t *testing.T) {
 	bls.SetFr(&s, "1927409816240961209460912649124")
 	MakeVerkleProofOneLeaf(root, zeroKeyTest, &s)
 }
+
+func TestProofVerifyTwoLeaves(t *testing.T) {
+	root := New()
+	root.Insert(zeroKeyTest, testValue)
+	root.Insert(ffx32KeyTest, testValue)
+
+	// Calculate all commitments
+	s1, s2 := kzg.GenerateTestingSetup("1927409816240961209460912649124", 1024)
+	fftCfg := kzg.NewFFTSettings(10)
+	ks := kzg.NewKZGSettings(fftCfg, s1, s2)
+	var err error
+	lg1, err = fftCfg.FFTG1(s1, true)
+	if err != nil {
+		panic(err)
+	}
+	_ = root.ComputeCommitment(ks, lg1)
+
+	var s bls.Fr
+	bls.SetFr(&s, "1927409816240961209460912649124")
+	comms, y, _, d, pi, rho, zis, yis := MakeVerkleProofOneLeaf(root, zeroKeyTest, &s)
+
+	if !VerifyVerkleProof(d, pi, rho, &y, comms, zis, yis, &s2[1]) {
+		t.Fatal("proof verification failed")
+	}
+}
