b87a0f8cb2aa201e1ff2fbef79ce8f07dc95013a
Sina Mahmoodi <itz.s1na@gmail.com>
2021-05-06 13:12:27 +0200

    Pass width to parse node instead of TC

--- encoding.go
+++ encoding.go
@@ -9,7 +9,7 @@ import (
 
 const ErrInvalidNodeEncoding = "invalid node encoding"
 
-func ParseNode(serialized []byte, depth int, tc *TreeConfig) (VerkleNode, error) {
+func ParseNode(serialized []byte, depth, width int) (VerkleNode, error) {
 	elems, _, err := rlp.SplitList(serialized)
 	if err != nil {
 		return nil, err
@@ -47,13 +47,16 @@ func ParseNode(serialized []byte, depth int, tc *TreeConfig) (VerkleNode, error)
 		if err != nil {
 			return nil, err
 		}
-		return createInternalNode(first, children, depth, tc)
+		return createInternalNode(first, children, depth, width)
 	default:
 		return nil, errors.New(ErrInvalidNodeEncoding)
 	}
 }
 
-func createInternalNode(bitlist []byte, raw []byte, depth int, tc *TreeConfig) (*InternalNode, error) {
+func createInternalNode(bitlist []byte, raw []byte, depth, width int) (*InternalNode, error) {
+	// GetTreeConfig caches computation result, hence
+	// this op has low overhead
+	tc := GetTreeConfig(width)
 	n := (newInternalNode(depth, tc)).(*InternalNode)
 	indices := indicesFromBitlist(bitlist)
 	if len(raw)/32 != len(indices) {
