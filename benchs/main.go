a5d5361c3ca98e4667e09eec76f2205d9a015841
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-08-18 14:21:52 +0200

    lint: remove dead code

--- benchs/main.go
+++ benchs/main.go
@@ -6,33 +6,7 @@ import (
 	"time"
 
 	"github.com/gballet/go-verkle"
-	"github.com/protolambda/go-kzg"
-	"github.com/protolambda/go-kzg/bls"
 )
-
-// GenerateTestingSetupWithLagrange creates a setup of n values from the given secret,
-// along with the  **for testing purposes only**
-func GenerateTestingSetupWithLagrange(secret string, n uint64, fftCfg *kzg.FFTSettings) ([]bls.G1Point, []bls.G2Point, []bls.G1Point, error) {
-	var s bls.Fr
-	bls.SetFr(&s, secret)
-
-	var sPow bls.Fr
-	bls.CopyFr(&sPow, &bls.ONE)
-
-	s1Out := make([]bls.G1Point, n)
-	s2Out := make([]bls.G2Point, n)
-	for i := uint64(0); i < n; i++ {
-		bls.MulG1(&s1Out[i], &bls.GenG1, &sPow)
-		bls.MulG2(&s2Out[i], &bls.GenG2, &sPow)
-		var tmp bls.Fr
-		bls.CopyFr(&tmp, &sPow)
-		bls.MulModFr(&sPow, &tmp, &s)
-	}
-
-	s1Lagrange, err := fftCfg.FFTG1(s1Out, true)
-
-	return s1Out, s2Out, s1Lagrange, err
-}
 
 func main() {
 	benchmarkInsertInExisting(8)
