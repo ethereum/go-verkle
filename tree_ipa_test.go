5668e5c6acfb556c73bc552be1442f85a483917b
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2021-12-21 18:48:25 +0100

    export FromBytes and FromLEBytes (#165)

--- tree_ipa_test.go
+++ tree_ipa_test.go
@@ -48,7 +48,7 @@ func extensionAndSuffixOneKey(key, value []byte, ret *Point) {
 		zero, t1, t2, c1                Point
 	)
 	stemComm0 := srs[0]
-	fromBytes(&v, key[:31])
+	FromBytes(&v, key[:31])
 	stemComm1.ScalarMul(&srs[1], &v)
 
 	leafToComms(vs[:], value)
@@ -142,7 +142,7 @@ func TestInsertSameStemTwoLeaves(t *testing.T) {
 	comm := root.ComputeCommitment()
 
 	stemComm0 := GetConfig().conf.SRS[0]
-	fromBytes(&v, key_a[:31])
+	FromBytes(&v, key_a[:31])
 	stemComm1.ScalarMul(&srs[1], &v)
 
 	leafToComms(vs[:], key_a)
