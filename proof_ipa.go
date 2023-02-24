2abe09acebffe8d06fb75e9a33a97c0a65b27f9a
Guillaume Ballet <3272758+gballet@users.noreply.github.com>
2023-02-24 14:55:55 +0100

    fix: add custom JSON serializers

--- proof_ipa.go
+++ proof_ipa.go
@@ -27,7 +27,10 @@ package verkle
 
 import (
 	"bytes"
+	"encoding/hex"
+	"encoding/json"
 	"errors"
+	"fmt"
 	"sort"
 	"unsafe"
 
@@ -43,12 +46,150 @@ type IPAProof struct {
 	FinalEvaluation [32]byte                  `json:"finalEvaluation"`
 }
 
+type ipaproofMarshaller struct {
+	CL              [IPA_PROOF_DEPTH]string `json:"cl"`
+	CR              [IPA_PROOF_DEPTH]string `json:"cr"`
+	FinalEvaluation string                  `json:"finalEvaluation"`
+}
+
+func (ipp *IPAProof) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&ipaproofMarshaller{
+		CL: [IPA_PROOF_DEPTH]string{
+			hex.EncodeToString(ipp.CL[0][:]),
+			hex.EncodeToString(ipp.CL[1][:]),
+			hex.EncodeToString(ipp.CL[2][:]),
+			hex.EncodeToString(ipp.CL[3][:]),
+			hex.EncodeToString(ipp.CL[4][:]),
+			hex.EncodeToString(ipp.CL[5][:]),
+			hex.EncodeToString(ipp.CL[6][:]),
+			hex.EncodeToString(ipp.CL[7][:]),
+		},
+		CR: [IPA_PROOF_DEPTH]string{
+			hex.EncodeToString(ipp.CR[0][:]),
+			hex.EncodeToString(ipp.CR[1][:]),
+			hex.EncodeToString(ipp.CR[2][:]),
+			hex.EncodeToString(ipp.CR[3][:]),
+			hex.EncodeToString(ipp.CR[4][:]),
+			hex.EncodeToString(ipp.CR[5][:]),
+			hex.EncodeToString(ipp.CR[6][:]),
+			hex.EncodeToString(ipp.CR[7][:]),
+		},
+		FinalEvaluation: hex.EncodeToString(ipp.FinalEvaluation[:]),
+	})
+}
+
+func (ipp *IPAProof) UnmarshalJSON(data []byte) error {
+	aux := &ipaproofMarshaller{}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	if len(aux.FinalEvaluation) != 64 {
+		return fmt.Errorf("invalid hex string for final evaluation: %s", aux.FinalEvaluation)
+	}
+
+	currentValueBytes, err := hex.DecodeString(aux.FinalEvaluation)
+	if err != nil {
+		return fmt.Errorf("error decoding hex string for current value: %v", err)
+	}
+	copy(ipp.FinalEvaluation[:], currentValueBytes)
+
+	for i := range ipp.CL {
+		if len(aux.CL[i]) != 64 {
+			return fmt.Errorf("invalid hex string for CL[%d]: %s", i, aux.CL[i])
+		}
+		val, err := hex.DecodeString(aux.CL[i])
+		if err != nil {
+			return fmt.Errorf("error decoding hex string for CL[%d]: %s", i, aux.CL[i])
+		}
+		copy(ipp.CL[i][:], val[:])
+		if len(aux.CR[i]) != 64 {
+			return fmt.Errorf("invalid hex string for CR[%d]: %s", i, aux.CR[i])
+		}
+		val, err = hex.DecodeString(aux.CR[i])
+		if err != nil {
+			return fmt.Errorf("error decoding hex string for CR[%d]: %s", i, aux.CR[i])
+		}
+		copy(ipp.CR[i][:], val[:])
+	}
+	copy(ipp.FinalEvaluation[:], currentValueBytes)
+
+	return nil
+}
+
 type VerkleProof struct {
 	OtherStems            [][31]byte `json:"otherStems"`
 	DepthExtensionPresent []byte     `json:"depthExtensionPresent"`
 	CommitmentsByPath     [][32]byte `json:"commitmentsByPath"`
 	D                     [32]byte   `json:"d"`
 	IPAProof              *IPAProof  `json:"ipa_proof"`
+}
+
+type verkleProofMarshaller struct {
+	OtherStems            []string  `json:"otherStems"`
+	DepthExtensionPresent string    `json:"depthExtensionPresent"`
+	CommitmentsByPath     []string  `json:"commitmentsByPath"`
+	D                     string    `json:"d"`
+	IPAProof              *IPAProof `json:"ipa_proof"`
+}
+
+func (vp *VerkleProof) MarshalJSON() ([]byte, error) {
+	aux := &verkleProofMarshaller{
+		OtherStems:            make([]string, len(vp.OtherStems)),
+		DepthExtensionPresent: hex.EncodeToString(vp.DepthExtensionPresent[:]),
+		CommitmentsByPath:     make([]string, len(vp.CommitmentsByPath)),
+		D:                     hex.EncodeToString(vp.D[:]),
+		IPAProof:              vp.IPAProof,
+	}
+
+	for i, s := range vp.OtherStems {
+		aux.OtherStems[i] = hex.EncodeToString(s[:])
+	}
+	for i, c := range vp.CommitmentsByPath {
+		aux.CommitmentsByPath[i] = hex.EncodeToString(c[:])
+	}
+	return json.Marshal(aux)
+}
+
+func (vp *VerkleProof) UnmarshalJSON(data []byte) error {
+	var aux verkleProofMarshaller
+	err := json.Unmarshal(data, &aux)
+	if err != nil {
+		return err
+	}
+
+	vp.DepthExtensionPresent, err = hex.DecodeString(aux.DepthExtensionPresent)
+	if err != nil {
+		return fmt.Errorf("error decoding hex string for depth and extention present: %v", err)
+	}
+
+	vp.CommitmentsByPath = make([][32]byte, len(aux.CommitmentsByPath))
+	for i, c := range aux.CommitmentsByPath {
+		val, err := hex.DecodeString(c)
+		if err != nil {
+			return fmt.Errorf("error decoding hex string for commitment #%d: %w", i, err)
+		}
+		copy(vp.CommitmentsByPath[i][:], val)
+	}
+
+	currentValueBytes, err := hex.DecodeString(aux.D)
+	if err != nil {
+		return fmt.Errorf("error decoding hex string for D: %w", err)
+	}
+	copy(vp.D[:], currentValueBytes)
+
+	vp.OtherStems = make([][31]byte, len(aux.OtherStems))
+	for i, c := range aux.OtherStems {
+		val, err := hex.DecodeString(c)
+		if err != nil {
+			return fmt.Errorf("error decoding hex string for other stem #%d: %w", i, err)
+		}
+		copy(vp.OtherStems[i][:], val)
+	}
+
+	vp.IPAProof = aux.IPAProof
+	return nil
 }
 
 type Proof struct {
@@ -63,6 +204,46 @@ type Proof struct {
 type SuffixStateDiff struct {
 	Suffix       byte      `json:"suffix"`
 	CurrentValue *[32]byte `json:"currentValue"`
+}
+
+type suffixStateDiffMarshaller struct {
+	Suffix       byte   `json:"suffix"`
+	CurrentValue string `json:"currentValue"`
+}
+
+func (ssd SuffixStateDiff) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&suffixStateDiffMarshaller{
+		Suffix:       ssd.Suffix,
+		CurrentValue: hex.EncodeToString(ssd.CurrentValue[:]),
+	})
+}
+
+func (ssd *SuffixStateDiff) UnmarshalJSON(data []byte) error {
+	aux := &suffixStateDiffMarshaller{
+		CurrentValue: "",
+	}
+
+	if err := json.Unmarshal(data, &aux); err != nil {
+		return err
+	}
+
+	if len(aux.CurrentValue) != 64 {
+		return fmt.Errorf("invalid hex string for current value: %s", aux.CurrentValue)
+	}
+
+	currentValueBytes, err := hex.DecodeString(aux.CurrentValue)
+	if err != nil {
+		return fmt.Errorf("error decoding hex string for current value: %v", err)
+	}
+
+	*ssd = SuffixStateDiff{
+		Suffix:       aux.Suffix,
+		CurrentValue: &[32]byte{},
+	}
+
+	copy(ssd.CurrentValue[:], currentValueBytes)
+
+	return nil
 }
 
 type SuffixStateDiffs []SuffixStateDiff
