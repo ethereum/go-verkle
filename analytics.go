e07ec064c5a14591a091771054bd1268b66b4852
Ignacio Hagopian <jsign.uy@gmail.com>
2023-10-24 11:01:09 -0300

    depth count
    Signed-off-by: Ignacio Hagopian <jsign.uy@gmail.com>

--- analytics.go
+++ analytics.go
@@ -4,10 +4,10 @@ import (
 	"fmt"
 )
 
-func TreeWitness(tree VerkleNode, resolver NodeResolverFn, stem []byte) (int, int, int, int, int, error) {
+func TreeWitness(tree VerkleNode, resolver NodeResolverFn, stem []byte, depthCount []uint64) (int, int, int, error) {
 	switch n := tree.(type) {
 	case *InternalNode:
-		var depthMin, depthMax, leafNodeCount, internalNodeCount, keyValueCount int
+		var leafNodeCount, internalNodeCount, keyValueCount int
 		for i := 0; i < 256; i++ {
 			stem := append(stem, byte(i))
 			switch n.children[i].(type) {
@@ -16,40 +16,35 @@ func TreeWitness(tree VerkleNode, resolver NodeResolverFn, stem []byte) (int, in
 			case HashedNode:
 				serialized, err := resolver(stem[:n.depth+1])
 				if err != nil {
-					return 0, 0, 0, 0, 0, fmt.Errorf("verkle tree: error resolving node %x at depth %d: %w", stem, n.depth, err)
+					return 0, 0, 0, fmt.Errorf("verkle tree: error resolving node %x at depth %d: %w", stem, n.depth, err)
 				}
 				resolved, err := ParseNode(serialized, n.depth+1)
 				if err != nil {
-					return 0, 0, 0, 0, 0, fmt.Errorf("verkle tree: error parsing resolved node %x: %w", stem, err)
+					return 0, 0, 0, fmt.Errorf("verkle tree: error parsing resolved node %x: %w", stem, err)
 				}
 				n.children[i] = resolved
 			}
 
-			branchDepthMin, branchDepthMax, branchLeafNodeCount, branchInternalNodeCount, branchKeyValueCount, err := TreeWitness(n.children[i], resolver, stem)
+			branchLeafNodeCount, branchInternalNodeCount, branchKeyValueCount, err := TreeWitness(n.children[i], resolver, stem, depthCount)
 			if err != nil {
-				return 0, 0, 0, 0, 0, fmt.Errorf("failed to get witness for branch %d: %w", i, err)
-			}
-			if depthMin > branchDepthMin+1 {
-				depthMin = branchDepthMin + 1
-			}
-			if depthMax < branchDepthMax+1 {
-				depthMax = branchDepthMax + 1
+				return 0, 0, 0, fmt.Errorf("failed to get witness for branch %d: %w", i, err)
 			}
 			leafNodeCount += branchLeafNodeCount
 			internalNodeCount += branchInternalNodeCount
 			keyValueCount += branchKeyValueCount
 
 		}
-		return depthMin, depthMax, leafNodeCount, internalNodeCount + 1, keyValueCount, nil
+		return leafNodeCount, internalNodeCount + 1, keyValueCount, nil
 	case *LeafNode:
+		depthCount[n.depth]++
 		var keyValueCount int
 		for i := 0; i < 256; i++ {
 			if n.values[i] != nil {
 				keyValueCount++
 			}
 		}
-		return 0, 0, 1, 0, keyValueCount, nil
+		return 1, 0, keyValueCount, nil
 	default:
-		return 0, 0, 0, 0, 0, fmt.Errorf("unknown node type: %T", n)
+		return 0, 0, 0, fmt.Errorf("unknown node type: %T", n)
 	}
 }
